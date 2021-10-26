0cbd2cbaefba83430b9288b935032ca80c821191
a.mironnikov <a.mironnikov@ngslab.ru>
2021-10-26 21:40:54 +0300

    Chapter 3. Selection sort

--- src/main/java/ru/edu/mironnikov/SelectionSort.java
+++ src/main/java/ru/edu/mironnikov/SelectionSort.java
@@ -8,12 +8,16 @@ package ru.edu.mironnikov;
 public interface SelectionSort {
     static int[] sort(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
+            int minElementIndex = i;
             for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[j] < numbers[i]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[i];
-                    numbers[i] = temp;
+                if (numbers[minElementIndex] > numbers[j]) {
+                    minElementIndex = j;
                 }
+            }
+            if (minElementIndex != i) {
+                int temp = numbers[i];
+                numbers[i] = numbers[minElementIndex];
+                numbers[minElementIndex] = temp;
             }
         }
         return numbers;
